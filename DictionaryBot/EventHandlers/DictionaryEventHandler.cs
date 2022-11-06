264557bc0d1a015c0f0461d8479b979fc0bbb83c
JulianusIV <ivjulianus@gmail.com>
2022-11-06 18:41:39 +0100

    Basic framework

---
+++ DictionaryBot/EventHandlers/DictionaryEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace DictionaryBot.EventHandlers
+{
+    internal class DictionaryEventHandler
+    {
+        internal static Task MessageCreated(DiscordClient sender, MessageCreateEventArgs e)
+        {
+            _ = Task.Run(() =>
+            {
+                //ignore messages with a certain prefix
+
+                //call API to validate word (https://dictionaryapi.dev/)
+
+                //check database for repeated words
+
+                //react with a checkmark or send a message and delete the word
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
--- DictionaryBot/Program.cs
+++ DictionaryBot/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DictionaryBot.EventHandlers;
+using DSharpPlus;
+
+#if DEBUG
+foreach (var line in File.ReadAllLines("settings.env"))
+{
+    Environment.SetEnvironmentVariable(line[..line.IndexOf('=')], line[(line.IndexOf('=') + 1)..]);
+}
+#endif
+
+DiscordClient client = new(new DiscordConfiguration()
+{
+    Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new Exception("Please set DISCORD_TOKEN EnvVar!"),
+    Intents = DiscordIntents.MessageContents
+});
+
+client.MessageCreated += DictionaryEventHandler.MessageCreated;
+
+await client.ConnectAsync();
+
+await Task.Delay(-1);
